3b629792b967a40beea85b24ff88eca793f26b19
GuilhermeSDias <guilhermedude.7@gmail.com>
2017-02-13 21:55:35 -0200

    Imagem da Camera

--- app/src/main/java/com/example/cpdbm03/horabus/FormularioEmpresa.java
+++ app/src/main/java/com/example/cpdbm03/horabus/FormularioEmpresa.java
@@ -1,33 +1,68 @@
 package com.example.cpdbm03.horabus;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.cpdbm03.horabus.dao.EmpresaDAO;
 import com.example.cpdbm03.horabus.modelo.Empresa;
 
+import java.io.File;
+
+import static android.R.attr.data;
+
 
 public class FormularioEmpresa extends AppCompatActivity {
 
+    public static final int CODIGO_CAMERA = 567;
     private FormularioEmpresaHelper helper;
+    private String caminhoFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_empresa);
 
-        helper = new FormularioEmpresaHelper(this, campoFoto);
+
+        helper = new FormularioEmpresaHelper(this);
 
         Intent intent = getIntent();
         Empresa empresa = (Empresa) intent.getSerializableExtra("empresa");
-        if(empresa != null) {
+        if (empresa != null) {
             helper.preencheFormularioEmpresa(empresa);
         }
+
+        Button botaoFoto = (Button) findViewById(R.id.formulario_botao_foto);
+        botaoFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                caminhoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis() + ".jpg";
+                File arquivoFoto = new File(caminhoFoto);
+                intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(arquivoFoto));
+                startActivityForResult(intentCamera, CODIGO_CAMERA);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CODIGO_CAMERA) {
+                helper.carregaImagem(caminhoFoto);
+            }
+        }
+
     }
 
     @Override
